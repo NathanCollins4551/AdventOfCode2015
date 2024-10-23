bdb85ed80b5aee2b13f9346510917774c94f5b4a
NathanCollins4551 <162639301+NathanCollins4551@users.noreply.github.com>
2024-10-22 22:35:22 -0400

    test

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,10 +1,10 @@
 ﻿namespace AdventOfCode;
 
-public class Day01 : BaseDay
+public class Day_01 : BaseDay
 {
     private readonly string _input;
 
-    public Day01()
+    public Day_01()
     {
         _input = File.ReadAllText(InputFilePath);
     }
