90e7c26c5a2c85101a60aa2154b2ac935646376f
NathanCollins4551 <162639301+NathanCollins4551@users.noreply.github.com>
2024-10-23 16:01:43 -0400

    Day_01 and Day_02 added

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -9,7 +9,48 @@ public class Day_01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{SolvePartOne()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new($"{SolvePartTwo()}");
+
+   
+    
+    public int SolvePartOne()
+    {
+        int floor = 0;
+        foreach (char x in _input)
+        {
+            if (x == '(')
+            {
+                floor++;
+            }
+            else if (x == ')')
+            {
+                floor--;
+            }
+        }
+        return floor;
+    }
+    
+public int SolvePartTwo()
+    {
+        int floor = 0;
+        foreach (var x in _input.Select((ch,index) => (ch, index)))
+        {
+            if (x.ch == '(')
+            {
+                floor++;
+            }
+            else if (x.ch == ')')
+            {
+                floor--;
+            }
+            if(floor == -1)
+            {
+                return x.index +1;
+            }
+        }
+        return -1;
+    }
 }
+
---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode;
+
+public class Day_02 : BaseDay
+{
+    private readonly string _input;
+
+    public Day_02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{SolvePartOne()}");
+
+    public override ValueTask<string> Solve_2() => new($"{SolvePartTwo()}");
+
+
+
+    public int SolvePartOne()
+    {
+        return _input
+            .Split('\n')
+            .Where(dimensions => dimensions.Length > 0)
+            .Select(dimensions => dimensions.Split('x')
+                                            .Select(num => Int32.Parse(num))
+                                            .Order()
+                                            .ToArray()) 
+            .Select(d => {
+                return ((3 * d[0] * d[1]) + (2 * d[0] * d[2]) + (2 * d[1] * d[2]));
+            })
+            .Sum();
+    }
+
+    public int SolvePartTwo()
+    {
+        return _input
+            .Split('\n')
+            .Where(dimensions => dimensions.Length > 0)
+            .Select(dimensions => dimensions.Split('x')
+                                            .Select(num => Int32.Parse(num))
+                                            .Order()
+                                            .ToArray())
+            .Select(d => {
+                return ((2 * d[0]) + (2 * d[1]) + (d[0] * d[1] * d[2]));
+            })
+            .Sum();
+    }
+
+
+
+
+
+}
+
