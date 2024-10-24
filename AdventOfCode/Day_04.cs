f0e1e6e3c5032c6e5e1d31685e8905432131f283
NathanCollins4551 <162639301+NathanCollins4551@users.noreply.github.com>
2024-10-23 21:36:31 -0400

    up to day 5 complete, will need impoved(cleaned up, commented, omptimized)

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode;
+
+public class Day_04 : BaseDay
+{
+    private readonly string input;
+
+    public Day_04()
+    {
+        input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{SolvePartOne()}");
+
+    public override ValueTask<string> Solve_2() => new($"{SolvePartTwo()}");
+
+
+
+    public int SolvePartOne()
+    {
+        int num = 0;
+        while (true)
+        {
+            string md5 = GenerateMD5(input+num.ToString());
+            if (md5.Substring(0,5) == "00000")
+            {
+              return num;
+            }
+            else
+            {
+                num++;
+            }
+        }
+    } 
+
+    public int SolvePartTwo()
+    {
+        int num = 0;
+        while (true)
+        {
+            string md5 = GenerateMD5(input + num.ToString());
+            if (md5.Substring(0, 6) == "000000")
+            {
+                return num;
+            }
+            else
+            {
+                num++;
+            }
+        }
+    }
+
+    public string GenerateMD5(string input)
+    {
+        MD5 md5Hasher = MD5.Create();
+        byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+        StringBuilder sBuilder = new StringBuilder();
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+        return sBuilder.ToString();
+    }
+}
+
---
+++ AdventOfCode/Day_05.cs
@@ -0,0 +1,135 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode;
+
+public class Day_05 : BaseDay
+{
+    private readonly string input;
+
+    public Day_05()
+    {
+        input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{SolvePartOne()}");
+
+    public override ValueTask<string> Solve_2() => new($"{SolvePartTwo()}");
+
+
+
+    public int SolvePartOne()
+    {
+        return input
+            .Split("\n")
+            .Where(line => line.Length > 0)
+            .Select(name => isNicePartOne(name))
+            .Sum();
+    }
+
+    public int SolvePartTwo()
+    {
+        return input
+            .Split("\n")
+            .Where(line => line.Length > 0)
+            .Select(name => isNicePartTwo(name))
+            .Sum();
+    }
+
+    public int isNicePartOne(string name)
+    {
+        //check for ! double letter
+        if (!hasDoubleLetter(name))
+        {
+            return 0;
+        }
+        //check for illegal substrings
+        if (hasIllegalSubstring(name))
+        {
+            return 0;
+        }
+        //check for less than 3 vowels
+        if (numVowel(name) < 3)
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+    public int isNicePartTwo(string name)
+    {
+        //check for not repeating pair
+        if (!hasRepeatingPair(name))
+        {
+            return 0;
+        }
+        //check for not letter sandwhich, ex: aba, fnf
+        if (!hasLetterSandwhich(name))
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+    public bool hasDoubleLetter(string name)
+    {
+        for (int i = 0; i < name.Length -1; i++)
+        {
+            if (name[i] == name[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool hasIllegalSubstring(string name)
+    {
+        if (name.Contains("ab") || name.Contains("cd") || name.Contains("pq") || name.Contains("xy"))
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    public int numVowel(string name)
+    {
+        int num = 0;
+        foreach (char x in name)
+        {
+            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')
+            {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    public bool hasRepeatingPair(string name)
+    {
+        for(int i = 0; i < name.Length-1; i++)
+        {
+            string pair = name.Substring(i,2);
+            if (name.Substring(i + 2).Contains(pair))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool hasLetterSandwhich(string name)
+    {
+        if(name.Length > 2)
+        {
+            for (int i = 0; i < name.Length - 2; i++)
+            {
+                if (name[i] == name[i + 2]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
